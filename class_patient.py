2ce52a171e5e0aeb2fb008fa7e496bebaf68d776
Leafar Rasec <leafar006rasec@gmail.com>
2026-06-23 18:46:16 +0000

    initializing class patient

---
+++ class_patient.py
@@ -0,0 +1,9 @@
+class Patient:
+
+    def __init__(self, id, name, age, cpf, phone_number, report):
+        self.id = id
+        self.name = name
+        self.age = age
+        self.cpf = cpf
+        self.phone_number = phone_number
+        self.report = report
